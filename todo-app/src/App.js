81c6e2a392a8555d0d755c361c13639a7854df92
OkuhleM <lindokuhlea38@gmail.com>
2022-07-25 12:33:57 +0200

    added new components

---
+++ todo-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddToDo from './Components/AddToDo';
+
+function App() {
+  return (
+    <div className="App">
+     <AddToDo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-app/src/Components/RetrieveData.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RetrieveData() {
+  return (
+    <div>RetrieveData</div>
+  )
+}
+
+export default RetrieveData
